7cf1f2beab98b0f5e66af744a700badb8c83603d
user <a>
2016-12-01 09:40:51 +0800

    v1 UI

---
+++ app/src/main/java/com/org/iii/shine26/MainActivity.java
@@ -0,0 +1,49 @@
+package com.org.iii.shine26;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+    private GridView gridView;
+    private MyAdapter myAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = (GridView)findViewById(R.id.gridView);
+        initGridView();
+    }
+
+    private void initGridView(){
+        myAdapter = new MyAdapter();
+        gridView.setAdapter(myAdapter);
+    }
+    
+    private class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+}
